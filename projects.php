37ea095eab4a7be50e035feab232b650bd8c29be
Marcus Gibeson <133388744+MarcusGibeson@users.noreply.github.com>
2024-08-30 13:31:03 -0400

    commit

--- projects.php
+++ projects.php
@@ -12,7 +12,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <title>Document</title>
-    <link rel="stylesheet" href="css/project_grid.css">
+    <!-- <link rel="stylesheet" href="css/project_grid.css"> -->
 </head>
 <body>
 
